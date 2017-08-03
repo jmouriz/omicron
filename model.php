83c0db704f9b0c3150b38aabd1e7c87e473f05b1
Juan Manuel Mouriz <jmouriz@gmail.com>
2017-08-03 02:26:02 -0300

    Documentación de la API

--- model.php
+++ model.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @package ORM
+ * @author Juan Manuel Mouriz <jmouriz@gmail.com>
+ * @copyright 2017 Juan Manuel Mouriz
+ * @license https://opensource.org/licenses/mit-license.php The MIT License
+ */
 namespace ORM;
 
 use \PDO;
@@ -7,13 +13,38 @@ $string = 'sqlite:database.db';
 $username = null;
 $password = null;
 
+/**
+ * Clase que representa un modelo de datos
+ */
 class Model {
-   protected $fields = array();
+   /**
+    * @ignore
+    */
    protected $connection;
+
+   /**
+    * @property array $fields Los campos de la tabla
+    */
+   protected $fields = array();
+
+   /**
+    * @property string $table El nombre de la tabla
+    */
    protected $table;
+
+   /**
+    * @ignore
+    */
    protected $data;
+
+   /**
+    * @property string $key La llave primaria que identifica cada fila
+    */
    protected $key;
 
+   /**
+    * @ignore
+    */
    public function __construct() {
       global $string, $database, $password;
       $options = array(
@@ -24,22 +55,36 @@ class Model {
       $this->data = (object) array();
    }
 
+   /**
+    * @ignore
+    */
    public function __destruct() {
       $this->connection = null;
    }
 
+   /**
+    * @ignore
+    */
    public function __set($key, $value) {
       if (in_array($key, $this->fields)) {
          $this->data->$key = $value;
       }
    }
 
+   /**
+    * @ignore
+    */
    public function __get($key) {
       if (property_exists($this->data, $key)) {
          return $this->data->$key;
       }
    }
 
+   /**
+    * Ejecuta una sentencia SQL
+    *
+    * @param string $sql La sentencia a ejecutar
+    */
    protected function query($sql) {
       $query = $this->connection->prepare($sql);
       foreach ($this->data as $key => $value) {
@@ -51,14 +96,32 @@ class Model {
       return $query;
    }
 
+   /**
+    * Devuelve la fila actual
+    *
+    * @return object La fila actual
+    */
    public function get() {
       return $this->data;
    }
 
+   /**
+    * Establece una columna de búsqueda diferente a la llave primaria
+    *
+    * @param string $key La columna de búsqueda
+    */
    public function index($key) {
       $this->key = $key;
    }
 
+   /**
+    * Establece o devuelve el valor único que identifica una fila,
+    * es decir, el valor de la llave primaria
+    *
+    * @param string|null $key El valor único que identifica la fila
+    *
+    * @return mixed El valor único que identifica la fila
+    */
    public function key($value = null) {
       $key = $this->key;
       if ($value === null) {
@@ -68,11 +131,23 @@ class Model {
       }
    }
 
+   /**
+    * Selecciona todos los registros de la tabla
+    *
+    * @return array Todos los registros de la tabla
+    */
    public function all() {
       $query = $this->query("select * from {$this->table}");
       return $query->fetchAll(PDO::FETCH_OBJ);
    }
 
+   /**
+    * Selecciona un registro
+    *
+    * @param mixed $key El valor de la clave primaria que identifica al registro
+    *
+    * @return object El registro cuya clave primaria sea $key
+    */
    public function select($key) {
       $this->key($key);
       $query = $this->query("select * from {$this->table} where {$this->key} = :{$this->key}");
@@ -83,6 +158,9 @@ class Model {
       return $data;
    }
 
+   /**
+    * Actualiza una fila
+    */
    public function update() {
       $set = array();
       foreach ($this->data as $key => $value) {
@@ -92,6 +170,9 @@ class Model {
       $this->query("update {$this->table} set $expression where {$this->key} = :{$this->key}");
    }
 
+   /**
+    * Inserta una fila
+    */
    public function insert() {
       $fields = implode(',', $this->fields);
       $values = implode(',:', $this->fields);
@@ -99,10 +180,18 @@ class Model {
       $this->key($this->connection->lastInsertId());
    }
 
+   /**
+    * Elimina una fila
+    */
    public function delete() {
       $this->query("delete from {$this->table} where {$this->key} = :{$this->key}");
    }
 
+   /**
+    * Actualiza o inserta una fila según exista un valor para la llave primaria.
+    * Si existe un valor para la llave primaria, el registro se actualiza, en
+    * caso contrario, se inserta
+    */
    public function save() {
       if ($this->key()) {
          $this->update();
